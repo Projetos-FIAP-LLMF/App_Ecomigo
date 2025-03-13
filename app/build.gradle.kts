9df6a6f7c625dbd04f5c9544863fb56872af16d7
Francine Maciel <francine_sa@hotmail.com>
2025-03-12 21:08:47 -0300

    Api google maps adicionada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
 }
 
 android {
@@ -62,4 +63,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation ("com.google.maps.android:maps-compose:2.11.4")
+    implementation ("com.google.android.gms:play-services-maps:18.1.0")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+
 }
--- app/src/main/java/com/example/ecomigo/ApimapsActivity.kt
+++ app/src/main/java/com/example/ecomigo/ApimapsActivity.kt
@@ -11,34 +11,60 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberMarkerState
 
 class ApimapsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                ButtonScreen()
+                MapScreen()
+
+               // ButtonScreen()
             }
         }
     }
 }
 
 @Composable
-fun ButtonScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        val context = LocalContext.current
-        Button(
-
-            onClick = {
-                val intent = Intent(context, SustainableActivity::class.java)
-                context.startActivity(intent) },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Ver Dicas")
-        }
+fun MapScreen() {
+    val saoPaulo = LatLng(-23.5505, -46.6333)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(saoPaulo, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ){
+        Marker(
+            state = rememberMarkerState(position = saoPaulo),
+            title = "São Paulo",
+            snippet = "Cidade maravilhosa"
+        )
     }
 }
+
+//@Composable
+//fun ButtonScreen() {
+//    Column(
+//        modifier = Modifier.fillMaxSize().padding(16.dp),
+//        verticalArrangement = Arrangement.Center
+//    ) {
+//        val context = LocalContext.current
+//        Button(
+//
+//            onClick = {
+//                val intent = Intent(context, SustainableActivity::class.java)
+//                context.startActivity(intent) },
+//            modifier = Modifier.fillMaxWidth()
+//        ) {
+//            Text("Ver Dicas")
+//        }
+//    }
+//}
 
